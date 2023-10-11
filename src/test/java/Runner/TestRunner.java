59470a5a327c5f8df7e494b29133800c808bab38
poonammirchandani123 <147245184+poonammirchandani123@users.noreply.github.com>
2023-10-11 19:03:44 +0100

    This is my 1stt commit

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,17 +1,10 @@
 package Runner;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        features = {"src/test/java/feature/demo.feature"}, glue = {"Steps"}, monochrome = true,
- plugin = {"pretty",
-         "html:target/cucumber-reports" ,"html:target/HtmlReports"})
-
-
+@CucumberOptions(features ={"src/test/java/features/login.feature"} , glue = {"steps"})
 public class TestRunner {
 }
-
-
-
